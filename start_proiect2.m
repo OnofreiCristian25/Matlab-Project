9a54227a0b8e34333f18853695c5f483ce5beac1
OnofreiCristian25 <92866710+OnofreiCristian25@users.noreply.github.com>
2023-03-19 20:58:04 +0200

    Add files via upload

---
+++ start_proiect2.m
@@ -0,0 +1,19 @@
+clear all;
+close;
+clc;
+        
+       
+E=15;
+C1=5e-9;
+C2=C1;
+R1=10e3;
+R2=R1;
+R3=2e3;
+R4=2e3;
+R5=3.6e3;
+D=1;
+rd1=500; %rd1 on
+
+interfata_proiect(E,C1,C2,R1,R2,R3,R4,R5,rd1,D)
+
+
